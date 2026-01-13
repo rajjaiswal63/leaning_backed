dec721f6720005ba286b09436eb3c358c9855d45
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-01-13 14:26:32 +0530

    Delete src/Day01 directory

--- src/Day01/Hello.java
+++
@@ -1,6 +0,0 @@
-package Day01;
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
--- src/Day01/variables.java
+++
@@ -1,6 +0,0 @@
-// void main() {
-//     System.out.println("hello hi");
-//     String name="rak";
-//     System.out.println(name);
-//
-// }
