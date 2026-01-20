d7884eb46c77613f6e8d475e1ce187d9ca4ae776
rajjaiswal6394@gmail.com <rajjaiswal6394@gmail.com>
2026-01-20 18:38:20 +0530

    new

---
+++ src/javaBasics/Challenge01UniversityIDCard.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+public class Challenge01UniversityIDCard {
+    public static void main(String[] args){
+        final String CollegeName="xyz college";
+        String StudentName="Name - Raj jaiswal";
+        int age=22;
+        String CourseName="cse";
+        System.out.println(CollegeName + "\n" + StudentName + "\n" + "Age - "+ age +"");
+    }
+
+
+}
