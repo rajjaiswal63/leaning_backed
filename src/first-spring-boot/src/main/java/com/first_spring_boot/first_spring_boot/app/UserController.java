d476fea0d320c81dc2ef1752faf59653c304352f
rajjaiswal6394@gmail.com <rajjaiswal6394@gmail.com>
2026-02-12 17:13:35 +0530

    first spring boot done

--- src/first-spring-boot/src/main/java/com/first_spring_boot/first_spring_boot/app/UserController.java
+++ src/first-spring-boot/src/main/java/com/first_spring_boot/first_spring_boot/app/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -49,5 +50,46 @@ public class UserController{
     @GetMapping
     public List<UsersInApp> getuser(){
         return new ArrayList<>(db.values());
+    }
+
+    @GetMapping("/{roll}")
+    public ResponseEntity<UsersInApp> getOneUser(@PathVariable int roll){
+        if (!db.containsKey(roll)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(db.get(roll));
+    }
+
+    @GetMapping("/{roll}/order/{orderid}")
+    public ResponseEntity<UsersInApp> getOneUser(
+            @PathVariable int roll,
+            @PathVariable int orderid
+    ){
+        System.out.println("orderid = "+ orderid);
+        if (!db.containsKey(roll)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(db.get(roll));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<UsersInApp>> search(
+            @RequestParam(required = false, defaultValue = "no name") String name){
+        System.out.println("searched name ="+name);
+        return ResponseEntity.ok(new ArrayList<>(db.values()));
+
+
+    }
+    // developed search api (same as previous)
+    @GetMapping("/search2")
+    public ResponseEntity<List<UsersInApp>> search2(
+            @RequestParam(required = false) String name){
+        System.out.println("searched name ="+name);
+        List<UsersInApp>users=db.values().stream()
+                .filter(u->u.getName().equalsIgnoreCase(name))
+                .toList();
+        return ResponseEntity.ok(users);
+
+
     }
 }
