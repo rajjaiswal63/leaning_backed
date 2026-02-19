52e39f24cbcc9d4462189ddbf7a01b4e0824153f
rajjaiswal6394@gmail.com <rajjaiswal6394@gmail.com>
2026-02-19 22:03:03 +0530

    jdbc added

---
+++ src/first-jdbc/src/main/java/org/example/firstjdbc/FirstJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.example.firstjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJdbcApplication.class, args);
+    }
+
+}
---
+++ src/first-jdbc/src/test/java/org/example/firstjdbc/FirstJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
